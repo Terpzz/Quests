68bfbccdc24df5e50358d67021d06adab782e0a2
BlueGamesNetworkPMMP <140811591+Terpzz@users.noreply.github.com>
2023-10-10 02:48:29 -0700

    Update QuestMain.php

--- src/skyss0fly/Quests/QuestMain.php
+++ src/skyss0fly/Quests/QuestMain.php
@@ -7,83 +7,87 @@ use pocketmine\utils\Config;
 use pocketmine\player\Player;
 use pocketmine\event\Listener;
 use pocketmine\Server;
+use pocketmine\command\ConsoleCommandSender;
 use skyss0fly\Quests\Commands\AddQuestsCommand;
 use BeeAZ\HumanNPC\HumanNPC;
 
 class QuestMain extends PluginBase implements Listener {
-	
-	public function onEnable(): void {
-	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-	    $this->registerCommands();
-		
-	    if(!is_dir($this->getDataFolder())){
-	       mkdir($this->getDataFolder());
-	    }
-		
-            $this->quests = new Config($this->getDataFolder() . "quests.yml", Config::YAML);
+    
+    private $quests;
 
-            $this->getServer()->getPluginManager()->registerEvents(new QuestsListener(), $this);
-            $this->getLogger()->info("QuestsAPI has been enabled!");
-	}
-
-    	public function registerCommands() {
-            $this->getServer()->getCommandMap()->registerAll("quests", [
-            	new AddQuestsCommand($this)
-            ]);
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->registerCommands();
+        
+        if (!is_dir($this->getDataFolder())) {
+            mkdir($this->getDataFolder());
         }
-	
-	public function getQuests() {
-		return $this->quests->getAll();
-	}
-	
-    	/**
-     	* @throws \JsonException
-     	*/
-	public function addQuest(Player $player, $questName, $questDescription, array $rewards) {
-		$playerName = $player->getName();
-		
-        	if ($this->quests->exists($questName)) {
-            	    $player->sendMessage("A quest with the named '" . $questName . "' already exists.");
-            	    return false;
-        	}
 
-        	if (empty($questName) || empty($questDescription || empty($rewards))) {
-                    $player->sendMessage("Quest data is incomplete. Provide a valid quest name, description and rewards.");
-                    return false;
-                }
+        $this->quests = new Config($this->getDataFolder() . "quests.yml", Config::YAML);
 
-		$this->quests->set($questName, [
-			"player" => $playerName,
-			"description" => $questDescription,
-			"rewards" => $rewards
-		]);
-		
-		$this->quests->save();
-		
-		$player->sendMessage("You have added a new quest: '$questName'!");
-		
-		$npc = new HumanNPC($player->getLocation(), $player->getSkin());
-		$npc->setNameTag("Quest NPC: $questName");
-		$npc->setInvisible(false);
-        	$npc->spawnTo($player);
-        	return true;
-	}
-	
+        $this->getServer()->getPluginManager()->registerEvents(new QuestsListener(), $this);
+        $this->getLogger()->info("QuestsAPI has been enabled!");
+    }
+
+    public function registerCommands() {
+        $this->getServer()->getCommandMap()->registerAll("quests", [
+            new AddQuestsCommand($this)
+        ]);
+    }
+
+    public function getQuests() {
+        return $this->quests->getAll();
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function addQuest(Player $player, $questName, $questDescription, array $rewards) {
+        $playerName = $player->getName();
+
+        if ($this->quests->exists($questName)) {
+            $player->sendMessage("A quest with the named '" . $questName . "' already exists.");
+            return false;
+        }
+
+        if (empty($questName) || empty($questDescription) || empty($rewards)) { // Fixed empty condition
+            $player->sendMessage("Quest data is incomplete. Provide a valid quest name, description, and rewards.");
+            return false;
+        }
+
+        $this->quests->set($questName, [
+            "player" => $playerName,
+            "description" => $questDescription,
+            "rewards" => $rewards
+        ]);
+
+        $this->quests->save();
+
+        $player->sendMessage("You have added a new quest: '$questName'!");
+
+        $npc = new HumanNPC($player->getLocation(), $player->getSkin());
+        $npc->setNameTag("Quest NPC: $questName");
+        $npc->setInvisible(false);
+        $npc->spawnTo($player);
+        return true;
+    }
+
     /**
      * @throws \JsonException
      */
     public function completeQuest(Player $player, $questName) {
-	$questData = $this->quests->get($questName);
+        $questData = $this->quests->get($questName);
 
-	if(!$questData){
-	   $player->sendMessage("This quest does not exist!");
-	   return;
-	}
-		
-	if($questData["player"] !== $player->getName()){
-	   $player->sendMessage("You can't complete this quest!");
-	   return;
-	}
+        if (!$questData) {
+            $player->sendMessage("This quest does not exist!");
+            return;
+        }
+
+        if ($questData["player"] !== $player->getName()) {
+            $player->sendMessage("You can't complete this quest!");
+            return;
+        }
+
         foreach ($questData["rewards"] as $reward) {
             $command = "give " . $player->getName() . " " . $reward;
             $commandSender = new ConsoleCommandSender($this->getServer(), $this->getServer()->getLanguage());
@@ -94,12 +98,12 @@ class QuestMain extends PluginBase implements Listener {
                 return;
             }
         }
-		
-	$this->quests->remove($questName);
-	$this->quests->save();
-		
-	$player->sendMessage("You have completed the quest '$questName'!");
-   }
+
+        $this->quests->remove($questName);
+        $this->quests->save();
+
+        $player->sendMessage("You have completed the quest '$questName'!");
+    }
 
     /**
      * @throws \JsonException
